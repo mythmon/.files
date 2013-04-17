4ad68e6ddfef3046bb2f8fc9d38800b3aa0ae697
Mike Cooper <mythmon@gmail.com>
2013-04-17 10:54:00 -0700

    Add no-op and verbose and quiet flags.

--- utils/install.py
+++ utils/install.py
@@ -4,9 +4,7 @@ import logging
 import os
 import re
 import sys
-
-
-logging.basicConfig(level=logging.DEBUG)
+import argparse
 
 
 def parse_map(map_str):
@@ -46,6 +44,7 @@ def map_file(file_map, d, f):
 
 def install_file(source, dest):
     dest = os.path.expanduser(dest)
+    logging.debug('Processing {}'.format(source))
 
     try:
         dirname = os.path.dirname(dest)
@@ -60,11 +59,12 @@ def install_file(source, dest):
         if os.path.samefile(source, dest):
             return True
         else:
-            print 'Not replacing existing file {} with {}.'.format(dest, source)
+            logging.warning('Not replacing existing file {} with {}.'.format(dest, source))
             return False
     else:
-        print 'Linking {} to {}'.format(source, dest)
-        os.link(source, dest)
+        logging.info('Linking {} to {}'.format(source, dest))
+        if not CONFIG.noop:
+            os.link(source, dest)
 
 
 class ChangeDir(object):
@@ -80,7 +80,36 @@ class ChangeDir(object):
         os.chdir(self.olddir)
 
 
+def clamp(n, bottom, top):
+    return min(max(bottom, n), top)
+
+
+CONFIG = None
+
+def loadConfig():
+    global CONFIG
+    parser = argparse.ArgumentParser(description='Install dotfiles.')
+    parser.add_argument('-n', '--noop', action='store_true')
+    parser.add_argument('-v', '--verbose', action='append_const', const=1)
+    parser.add_argument('-q', '--quiet', action='append_const', dest='verbose', const=-1)
+
+    opt = parser.parse_args()
+
+    opt.verbose = clamp(2 + sum(opt.verbose or [0]), 0, 4)
+
+    CONFIG = opt
+
+
 def main():
+    loadConfig()
+
+    log_levels = [logging.CRITICAL, logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
+    logging.basicConfig(level=log_levels[CONFIG.verbose])
+
+    if CONFIG.noop:
+        logging.info('Running in no-op mode')
+
+
     try:
         with open('map') as f:
             map_str = f.read()
@@ -95,9 +124,12 @@ def main():
             # Remove leading ./ or .
             root = re.sub(r'^./?', '', root)
             for f in files:
-                dest = map_file(file_map, root, f)
-                if dest is not None:
-                    install_file(os.path.join(root, f), dest)
+                try:
+                    dest = map_file(file_map, root, f)
+                    if dest is not None:
+                        install_file(os.path.join(root, f), dest)
+                except ValueError:
+                    logging.error('File "{}" does not match any rules.'.format(f))
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.dirname(__file__)))
